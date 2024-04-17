d4fb15dba18277ab83fbc09640d2c69bfd5a9091
Paul Wellner Bou <paul@wellnerbou.de>
2024-04-17 09:58:08 +0200

    Implement configuration options to limit depth of certain fields on server site to avoid too much data if using a lot of relations and/or parent-child relations

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -9,7 +9,23 @@ const getModelPopulationAttributes = (model) => {
   return model.attributes;
 };
 
-const getFullPopulateObject = (modelUid, maxDepth = 20, ignore) => {
+/**
+ * @typedef {Object} FieldPopulateConfiguration
+ * @property {string?} collectionName - optional, if not given, it will be considered for all collections
+ * @property {string} field
+ * @property {number} depth
+ * @exports FieldPopulateConfiguration
+ */
+
+/**
+ * @param {string} modelUid 
+ * @param {number} maxDepth 
+ * @param {string[]?} ignore 
+ * @param {FieldPopulateConfiguration[]?} fieldPopulateConfigurations 
+ * @param {boolean?} debug
+ * @returns 
+ */
+const getFullPopulateObject = (modelUid, maxDepth = 20, ignore, fieldPopulateConfigurations, debug = false) => {
   const skipCreatorFields = strapi.plugin('strapi-plugin-populate-deep')?.config('skipCreatorFields');
 
   if (maxDepth <= 1) {
@@ -21,25 +37,46 @@ const getFullPopulateObject = (modelUid, maxDepth = 20, ignore) => {
 
   const populate = {};
   const model = strapi.getModel(modelUid);
-  if (ignore && !ignore.includes(model.collectionName)) ignore.push(model.collectionName)
+
   for (const [key, value] of Object.entries(
     getModelPopulationAttributes(model)
   )) {
-    if (ignore?.includes(key)) continue
+    if (ignore?.includes(key) || ignore?.includes(model.collectionName + '.' + key)) {
+      debug && console.debug(`[strapi-plugin-populate-deep] DEBUG Ignoring collectionName: ${model.collectionName}, field: ${key}`)
+      console.debug({
+        'ignore?.includes(key)': ignore?.includes(key),
+        'ignore?.includes(model.collectionName.key)': ignore?.includes(model.collectionName + '.' + key),
+        ignore
+      })
+      continue
+    }
+
+    let depth = maxDepth
+    if (fieldPopulateConfigurations) {
+      debug && console.debug(`[strapi-plugin-populate-deep] DEBUG collectionName: ${model.collectionName}, field: ${key}`)
+      const fieldPopulateConfiguration = fieldPopulateConfigurations.find(f => f.field === key && (!f.collectionName || f.collectionName === model.collectionName))
+      if (fieldPopulateConfiguration) {
+        debug && console.debug(`[strapi-plugin-populate-deep] DEBUG Overriding depth for collectionName: ${model.collectionName}, field: ${key} to ${fieldPopulateConfiguration.depth}`)
+        depth = fieldPopulateConfiguration.depth
+      }
+    }
+
     if (value) {
       if (value.type === "component") {
-        populate[key] = getFullPopulateObject(value.component, maxDepth - 1);
-      } else if (value.type === "dynamiczone") {
+        populate[key] = getFullPopulateObject(value.component, depth - 1, ignore, fieldPopulateConfigurations, debug);
+      } else if (value.type === "dynamiczone" && depth > 1) {
         const dynamicPopulate = value.components.reduce((prev, cur) => {
-          const curPopulate = getFullPopulateObject(cur, maxDepth - 1);
+          const curPopulate = getFullPopulateObject(cur, depth - 1, ignore, fieldPopulateConfigurations, debug);
           return curPopulate === true ? prev : merge(prev, curPopulate);
         }, {});
         populate[key] = isEmpty(dynamicPopulate) ? true : dynamicPopulate;
-      } else if (value.type === "relation") {
+      } else if (value.type === "relation" && depth > 1) {
         const relationPopulate = getFullPopulateObject(
           value.target,
-          (key === 'localizations') && maxDepth > 2 ? 1 : maxDepth - 1,
-          ignore
+          (key === 'localizations') && depth > 2 ? 1 : depth - 1,
+          ignore,
+          fieldPopulateConfigurations,
+          debug,
         );
         if (relationPopulate) {
           populate[key] = relationPopulate;
